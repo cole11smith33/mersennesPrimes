b6d3c8e08ecfbdccd2c700291d13c1d19e2896b8
cole11smith33 <cole11smith33@gmail.com>
2025-04-25 16:45:29 -0400

    created function which finds MPs

--- BasicSearch.py
+++ BasicSearch.py
@@ -1,8 +1,23 @@
 import math
 
 def isPrime(num):
-    ceiling = sqrt(num).floor() # this avoids redundently checking over 
+    ceiling = math.floor(math.sqrt(num)) # this avoids redundently checking over 
     for i in range(2, ceiling):
         if num % i == 0:
             return False
     return True    
+
+def findPossibleMP(n):
+    return (2**n)-1
+
+def findMP(totalDesired):
+    n = 2
+    currAmount = 0
+    while currAmount < totalDesired:
+        if isPrime(findPossibleMP(n)):
+            print(str(currAmount + 1) + ": " + str(findPossibleMP(n)))
+            currAmount += 1
+        n += 1
+        
+            
+findMP(11)
